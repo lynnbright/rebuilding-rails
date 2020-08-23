bbb5f36ff8a2bc1787bbd355f206c123f1eac205
lynnchang <tv725987@gmail.com>
2020-08-24 00:15:20 +0800

    Write an unit test for practice

--- rulers/test/application_test.rb
+++ rulers/test/application_test.rb
@@ -15,6 +15,13 @@ class RulersAppTest < Minitest::Test
 
     assert last_response.ok?
     body = last_response.body
-  assert body["Hello"]
+    assert body["Hello"]
+  end
+
+  def test_response_is_ok_for_other_paths
+    get '/other_paths'
+
+    assert last_response.ok?
+    assert_equal last_response.body, 'Hello from Ruby on Rulers!'
   end
 end
